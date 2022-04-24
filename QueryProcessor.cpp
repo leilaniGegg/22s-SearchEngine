be73c81cf6e10d629284112b4f7d94e55892b6cd
leilaniGegg <60398961+leilaniGegg@users.noreply.github.com>
2022-04-23 23:11:01 -0500

    in query process still

--- QueryProcessor.cpp
+++ QueryProcessor.cpp
@@ -16,22 +16,60 @@ void QueryProcessor::findWord(const string& request, IndexHandler& indexer){
 void QueryProcessor::query(const string& request, IndexHandler& indexer){
     //Request looks like: "AND social network"
     set<Article> matches; //data type may change when hash table is made
-    string currOperator;  //could be AND, OR, NOT
+    //string currOperator;  //could be AND, OR, NOT
     stringstream inSS(request);
     string tempWord;
     while(getline(inSS, tempWord, ' ')){
         if(tempWord == "AND"){
-            currOperator = "AND";
+            string w1, w2;
+            getline(inSS, w1, ' ');
+            getline(inSS, w2, ' ');
+            addArticles(tempWord, w1, w2, matches, indexer);
         }
         else if(tempWord == "OR"){
-            currOperator = "OR";
+            string w1, w2;
+            getline(inSS, w1, ' ');
+            getline(inSS, w2, ' ');
+            addArticles(tempWord, w1, w2, matches, indexer);
         }
         else if(tempWord == "NOT"){
-            currOperator = "NOT";
+            string w1;
+            getline(inSS, w1, ' ');
+            removeArticles(w1, matches, indexer);
         }
         else{ //normal word
-
+            string w1;
+            getline(inSS, w1, ' ');
+            addArticle(w1, matches, indexer);
         }
     }
 
+}
+
+void QueryProcessor::addArticles(const string& op, const string& word1, const string& word2,
+                                 set<Article>& matches, IndexHandler& indexer){
+    if(op == "AND") {
+        vector<Article> *word1Articles = &(indexer.getWordIndex().insert(Word(word1))->getArticles());
+        vector<Article> *word2Articles = &(indexer.getWordIndex().insert(Word(word2))->getArticles());
+        set_intersection(word1Articles->begin(), word1Articles->end(), word2Articles->begin(), word2Articles->end(), matches.begin());
+    }
+    else if(op == "OR"){
+        vector<Article> *word1Articles = &(indexer.getWordIndex().insert(Word(word1))->getArticles());
+        vector<Article> *word2Articles = &(indexer.getWordIndex().insert(Word(word2))->getArticles());
+        set_union(word1Articles->begin(), word1Articles->end(), word2Articles->begin(), word2Articles->end(), matches.begin());
+    }
+}
+
+void QueryProcessor::addArticle(const string& word1, set<Article>& matches, IndexHandler& indexer){
+    //if single word query just add all the words articles to matches set
+     vector<Article>* temp = &(indexer.getWordIndex().insert(Word(word1))->getArticles());
+     for(int i = 0; i < temp->size(); i++){
+         matches.insert(temp->at(i));
+     }
+}
+
+void QueryProcessor::removeArticles(const string& word1, set<Article>& matches, IndexHandler& indexer){
+    //I think this will work, will find the difference and result in set with elements in matches but not in word1Articles
+    vector<Article> *word1Articles = &(indexer.getWordIndex().insert(Word(word1))->getArticles());
+    set_difference(matches.begin(), matches.end(), word1Articles->begin(), word1Articles->end(), matches.begin());
 }
--- QueryProcessor.h
+++ QueryProcessor.h
@@ -8,6 +8,7 @@
 #include <string>
 #include <set>
 #include <sstream>
+#include <algorithm>
 #include "IndexHandler.h"
 #include "Article.h"
 
@@ -19,5 +20,8 @@ private:
 public:
     void findWord(const string& request, IndexHandler& indexer); // for timing demo
     void query(const string& request, IndexHandler& indexer); //indexer has AVL tree for words and hash maps for person and orgs
+    void addArticles(const string& op, const string& word1, const string& word2, set<Article>& matches, IndexHandler& indexer);
+    void addArticle(const string& word1, set<Article>& matches, IndexHandler& indexer); //for single word queries
+    void removeArticles(const string& word1, set<Article>& matches, IndexHandler& indexer);
 };
 #endif //INC_22S_FINAL_PROJ_QUERYPROCESSOR_H
